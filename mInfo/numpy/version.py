b5f61dbc6dab67ddb5e5a7afba2f5e135da7630f
thedixieflatline <miamijoeboy@gmail.com>
2014-06-13 16:41:58 +1000

    First Commit
    Proof of concept not finished

---
+++ mInfo/numpy/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM NUMPY SETUP.PY
+short_version = '1.8.1'
+version = '1.8.1'
+full_version = '1.8.1'
+git_revision = 'e715bce009c05bbeb5819f2b1d0468c6b776e3e3'
+release = True
+
+if not release:
+    version = full_version
